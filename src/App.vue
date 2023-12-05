503965cb58e74ac9e060eeedb823b9ae2f71f945
Amyyyy <ningning.z@zymebalanz.com>
2023-12-05 12:10:51 -0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, computed } from 'vue'
+import Header from "./components/Header.vue"
+import Quality from './views/Quality.vue'
+import Home from './views/Home.vue'
+
+const routes = {
+  '/': Home,
+  '/quality':Quality
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+  console.log(window.location.hash)
+})
+
+const selectedTab=ref("Home")
+const currentView = computed(() => {
+  const path=currentPath.value.slice(1).split('#')[0]
+  switch(path.slice(1).toLocaleLowerCase()){
+    case "quality":selectedTab.value="Quality"
+    break;
+    default:selectedTab.value="Home"
+  }
+  return routes[path || '/']||Home
+})
+</script>
+
+<template>
+<div>
+  <Header />
+  <component :is="currentView" />
+</div>
+</template>
